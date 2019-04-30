b67c812488c7739cbbdc453e389538fc1777cf2c
matrayu <northridge614@gmail.com>
2019-04-30 06:16:56 -0700

    Update button.test.js

--- src/components/button.test.js
+++ src/components/button.test.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import renderer from 'react-test-renderer'
 
 import Button from './button';
 
 describe('<Button />', () => {
     it('Should call props.onClick when the button is clicked', () => {
-        const wrapper = shallow(<Button />);
+        const mockCallBack = jest.fn()
+        
+        const wrapper = shallow(<Button onClick={mockCallBack}>Ok!</Button>);
         wrapper.find('button').simulate('click');
+       /*  expect(mockCallBack.mock.calls.length).toEqual(1); */
+    });
+
+    it('renders the UI as expected', () => {
+        const mockCallBack = jest.fn();
+
+        const tree = renderer
+            .create(<Button onClick={mockCallBack}>Ok!</Button>)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
     });
 });
